547fe620d5be61fdf25957ef2b3c6f95db00908f
Brady Ray <Brady Ray>
2025-11-17 14:11:12 -0500

    New update

--- squish.py
+++ squish.py
@@ -4,4 +4,7 @@ import scipy
 from scipy.stats import norm
 from fitting_functions import *
 data = np.loadtxt("Marshmallow.csv", delimiter=",", dtype=str)
-print(data[0,:])
+x = data[1:, 0].astype(np.float32)
+y = data[1:, 1].astype(np.float32)
+print('y = ', y)
+print('x = ', x)
